1a91cc6b4a8d807e897fbeb2a1a35828b3d75f43
sarahvar <sarahvarin95@gmail.com>
2023-06-16 21:57:24 +0200

    première validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close").addEventListener("click", closeModal)
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,50 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Fermeture de la modale
+function closeModal(){
+  modalbg.style.display = "none";
+}
+function modalValidation(){
+  const modalSuccess = document.querySelector("#modalSuccess");
+  modalSuccess.style.display="block";
+  modalbg.style.display="none";
+}
+function error(message){
+  return message;
+}
 
+//Test du champs firstname (prénoms)
+function firstnameValidation() {
+  let firstName = document.querySelector("#first").value;
+  const firstnameError = document.getElementById("firstNameErrorMsg");
+  if (firstName === "") {
+    firstnameError.innerText = error ("Champs obligatoire ");
+    firstnameError.style.color = "red";
+    //firstnameError.style.paddingTop = "14px";
+    firstnameError.style.fontSize = "25px"
+    return false;
+  } else if (firstName.trim().length < 2) {
+    firstnameError.innerText = error ("Veuillez entrer 2 caractères ou plus");
+    firstnameError.style.color = "red";
+    firstnameError.style.fontSize = "25px"
+
+    return false;
+}else {
+  firstnameError.innerText= error("")
+  return true
+}
+}
+function validate(){
+  return(
+    firstnameValidation()
+  )
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", (event)=>{
+  event.preventDefault()
+  if (validate() == true ){
+    modalValidation()
+  } else {return false};
+})

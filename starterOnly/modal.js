e8ed598d03b85a37825585b26c8809875e1aeb85
sarahvar <sarahvarin95@gmail.com>
2023-07-05 01:13:41 +0200

    modifs-soutenance

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,7 +33,7 @@ function closeModal() {
   modalSuccess.style.display = "none";
   location.reload();
 }
-// Modal de succès
+// Modale de validation
 function modalValidation() {
   const modalSuccess = document.querySelector("#modalSuccess");
   modalSuccess.style.display = "block";
@@ -43,6 +43,7 @@ function modalValidation() {
 function error(message) {
   return message;
 }
+
 //Test du champs firstname (prénoms)
 function firstnameValidation() {
   let firstName = document.querySelector("#first").value;
@@ -54,15 +55,15 @@ function firstnameValidation() {
     firstnameError.style.fontSize = "25px"
     return false;
   } else if (firstName.trim().length < 2) {
-    firstnameError.innerText = error ("Veuillez entrer 2 caractères ou plus");
+    firstnameError.innerText = error("Veuillez entrer 2 caractères ou plus");
     firstnameError.style.color = "red";
-    firstnameError.style.fontSize = "25px"
+    firstnameError.style.fontSize = "25px";
 
     return false;
-}else {
-  firstnameError.innerText= error("")
-  return true
-}
+  } else {
+    firstnameError.innerText = error("");
+    return true;
+  }
 }
 
 //Test du champs lastname (nom de famille)
@@ -76,43 +77,41 @@ function lastnameValidation() {
     lastnameError.style.fontSize = "25px"
     return false;
   } else if (lastName.trim().length < 2) {
-    lastnameError.innerText = error ("Veuillez entrer 2 caractères ou plus");
+    lastnameError.innerText = error("Veuillez entrer 2 caractères ou plus");
     lastnameError.style.color = "red";
-    lastnameError.style.fontSize = "25px"
+    lastnameError.style.fontSize = "25px";
 
     return false;
-}else {
-  lastnameError.innerText= error("")
-  return true
-}
+  } else {
+    lastnameError.innerText = error("");
+    return true;
+  }
 }
 //Test du champs email
 function emailValidation() {
   let email = document.querySelector("#email").value;
   let regexEmail = /.+\@.+\..+/;
-  const emailError= document.getElementById("emailErrorMsg");
-  if (email === ""){
-  emailError.innerText = error ("Champs obligatoire ");
-  emailError.style.color = "red";
-  emailError.style.fontSize = "25px"
-  
-  return false;
-}
-  else if (regexEmail.test(email) === false) {
-    emailError.innerText = error ("Merci d'inscrire une adresse mail correcte");
+  const emailError = document.getElementById("emailErrorMsg");
+  if (email === "") {
+    emailError.innerText = error("Champs obligatoire ");
     emailError.style.color = "red";
-    emailError.style.fontSize = "25px"
+    emailError.style.fontSize = "25px";
+
     return false;
+  } else if (regexEmail.test(email) === false) {
+    emailError.innerText = error("Merci d'inscrire une adresse mail correcte");
+    emailError.style.color = "red";
+    emailError.style.fontSize = "25px";
+    return false;
+  } else {
+    emailError.innerText = error("");
+    return true;
   }
-else{
-  emailError.innerText = error("")
-  return true;
-}
 }
 
 //Test du champs birthdate (date de naissance)
 function birthdateValidation() {
-  let birthCheckValue = document.querySelector('#birthdate').value;
+  let birthCheckValue = document.querySelector("#birthdate").value;
   const birthdateError = document.getElementById("birthdateErrorMsg");
   const minimumAge = 12;
   const today = new Date();
@@ -126,34 +125,32 @@ function birthdateValidation() {
       ? ageInYears - 1
       : ageInYears;
 
-  let errorMessage = '';
+  let errorMessage = "";
 
-  if (birthCheckValue === '') {
-    errorMessage = 'Merci de renseigner une date de naissance';
+  if (birthCheckValue === "") {
+    errorMessage = "Merci de renseigner une date de naissance";
     birthdateError.style.color = "red";
-    birthdateError.style.fontSize = "25px"
+    birthdateError.style.fontSize = "25px";
   } else if (ageInYears < minimumAge) {
     errorMessage = "Désolé(e), l'âge minimum requis est de 12 ans";
     birthdateError.style.color = "red";
-    birthdateError.style.fontSize = "25px"
+    birthdateError.style.fontSize = "25px";
   }
 
   birthdateError.innerText = errorMessage;
   birthdateError.innerHTML = errorMessage;
 
-  
-// Renvoie vrai si aucun message d'erreur n'est défini, ou faux s'il y en a un
-  return errorMessage === '';
+  // Renvoie vrai si aucun message d'erreur n'est défini, ou faux s'il y en a un
+  return errorMessage === "";
 }
 
 //Test du champs quantity (quantité de tournois)
 function quantityTournamentsValidation() {
   let quantity = document.querySelector("#quantity").value;
-  const quantityTournamentsError = document.getElementById("quantityTournamentsErrorMsg");
-  const quantityValid =
-    quantity >= 0 &&
-    quantity <= 99 &&
-    quantity !== "";
+  const quantityTournamentsError = document.getElementById(
+    "quantityTournamentsErrorMsg"
+  );
+  const quantityValid = quantity >= 0 && quantity <= 99 && quantity !== "";
 
   quantityTournamentsError.innerHTML = quantityValid
     ? ""
@@ -170,40 +167,53 @@ function quantityTournamentsValidation() {
     return false;
   }
 }
-//Test si une case du choix d'une ville a bien été cocher 
-function locationsValidation(){
+//Test si une case du choix d'une ville a bien été cocher
+function locationsValidation() {
   let locations = document.querySelector("#locationsErrorMsg");
-  const locationsError = document.getElementById("locationsErrorMsg")
-  const locationsCheck = document.querySelector('input[name="location"]:checked')
-  if (!locationsCheck){
-    locations.innerHTML = "Merci de choisir une ville"
+  const locationsError = document.getElementById("locationsErrorMsg");
+  const locationsCheck = document.querySelector(
+    'input[name="location"]:checked'
+  );
+  if (!locationsCheck) {
+    locations.innerHTML = "Merci de choisir une ville";
     locationsError.style.color = "red";
-    locationsError.style.fontSize = "25px"
-    return false
+    locationsError.style.fontSize = "25px";
+    return false;
   } else {
-      location.innerHTML = ""
-      return true
+    location.innerHTML = "";
+    return true;
   }
 }
-  
-function acceptedConditionsValidation(){
+
+function acceptedConditionsValidation() {
   const checkbox1 = document.querySelector("#checkbox1");
   const acceptedError = document.querySelector("#acceptedErrorMsg");
-  if(checkbox1.checked === false){
-    acceptedError.innerText = error ("Merci de lire et d'accepter les conditions d'utilisation")
+  if (checkbox1.checked === false) {
+    acceptedError.innerText = error(
+      "Merci de lire et d'accepter les conditions d'utilisation"
+    );
     acceptedError.style.color = "red";
-    acceptedError.style.fontSize ="25px"
-    return false
+    acceptedError.style.fontSize = "25px";
+    return false;
   } else {
-    acceptedError.innerText = error ("")
+    acceptedError.innerText = error("");
     acceptedError.style.color = "red";
-    acceptedError.style.fontSize = "25px"
+    acceptedError.style.fontSize = "25px";
     return true;
   }
 }
 //Fonction qui vérifie que tous les champs du formulaire sont true en verifiant la valeur retournée par chaque foncction
-function validate(){
-  return(
+function checkValidateAll() {
+  firstnameValidation();
+  lastnameValidation();
+  emailValidation();
+  birthdateValidation();
+  quantityTournamentsValidation();
+  locationsValidation();
+  acceptedConditionsValidation();
+}
+function validate() {
+  if (
     firstnameValidation() &&
     lastnameValidation() &&
     emailValidation() &&
@@ -211,15 +221,19 @@ function validate(){
     quantityTournamentsValidation() &&
     locationsValidation() &&
     acceptedConditionsValidation()
-  )
+  ) {
+    return true;
+  }
+  return false;
 }
-
 
 //Event au click submit pour permettre de valider le formulaire si tout est true
 const form = document.getElementById("form");
-form.addEventListener("submit", (event)=>{
-  event.preventDefault()
-  if (validate() == true ){
-    modalValidation()
-  } else {return false};
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (validate()) {
+    modalValidation();
+  } else {
+    checkValidateAll();
+  }
+});
